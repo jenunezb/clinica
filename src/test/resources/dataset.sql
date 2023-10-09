c5aea09be06d679228d516822873eb19079718a6
jenunezb <jenunezb@uqvirtual.edu.co>
2023-10-09 16:25:12 -0500

    10 pacientes para el dataset.sql

--- src/test/resources/dataset.sql
+++ src/test/resources/dataset.sql
@@ -2,7 +2,13 @@ insert into administrador values(1, 'pepito@email.com', '123');
 insert into administrador values(2, 'juanita@email.com', '234');
 
 
-# insert into paciente values('24567234', 0, 'Pepito Perez', '5454545', 'url_foto', 'Sin
-# alergias', 1, '1990-01-01', 1, 1);
-# insert into paciente values('18635123', 0, 'Juanita Lopez', '4564545', 'url_foto', 'Sin
-# alergias', 0, '1995-11-28', 1, 2);
+insert into paciente values(1, 'juanitalopez@gmail.com', '1', 5, false, 'url_fot', 'juanita lopez', '345154888', 'Sin alergias', 2, '1995-11-28', 1);
+insert into paciente values(2, 'pedrogonzalez@gmail.com', '2', 3, true, 'url_foto_2', 'pedro gonzalez', '555123456', 'alergia al polen', 1, '1990-08-15', 1);
+insert into paciente values(3, 'mariaperez@gmail.com', '3', 4, false, 'url_foto_3', 'maria perez', '789987654', 'sin alergias conocidas', 3, '1988-03-20', 2);
+insert into paciente values(4, 'carlosrodriguez@gmail.com', '4', 2, true, 'url_foto_4', 'carlos rodriguez', '654789321', 'alergia a los frutos secos', 1, '2000-05-10', 3);
+insert into paciente values(5, 'lauragonzalez@gmail.com', '5', 1, false, 'url_foto_5', 'laura gonzalez', '987654321', 'Sin alergias', 2, '1998-12-05', 2);
+insert into paciente values(6, 'andresramirez@gmail.com', '6', 3, false, 'url_foto_6', 'andres ramirez', '123456789', 'Sin alergias', 1, '1992-07-22', 1);
+insert into paciente values(7, 'sarasanchez@gmail.com', '7', 4, true, 'url_foto_7', 'sara sanchez', '567890123', 'alergia al gluten', 2, '1985-04-17', 3);
+insert into paciente values(8, 'davidlopez@gmail.com', '8', 5, false, 'url_foto_8', 'david lopez', '789012345', 'Sin alergias', 1, '1980-09-30', 2);
+insert into paciente values(9, 'anamartinez@gmail.com', '9', 2, false, 'url_foto_9', 'ana martinez', '234567890', 'Sin alergias', 3, '1997-02-14', 3);
+insert into paciente values(10, 'josehernandez@gmail.com', '10', 1, true, 'url_foto_10', 'jose hernandez', '456789012', 'alergia al marisco', 2, '1993-06-08', 2);
