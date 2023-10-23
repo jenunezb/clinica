9d72e0e7ccd7f72a88fe4dc8b052086ece3b5a52
jenunezb <jenunezb@uqvirtual.edu.co>
2023-10-23 18:59:49 -0500

    organización de PacienteController

--- src/main/java/co/edu/uniquindio/proyecto/controladores/PacienteController.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/PacienteController.java
@@ -24,78 +24,33 @@ public class PacienteController {
     }
 
     @PutMapping
-    public ResponseEntity<MensajeDTO> editarPerfil(@Valid @RequestBody DetallePacienteDTO detallePacienteDTO) throws Exception {
-        try {
-            int codigon = pacienteServicio.editarPerfil(detallePacienteDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new MensajeDTO(
-                    false, "Paciente " + codigon + " modificado correctamente"));
-        } catch (Exception e) {
-            // Maneja la excepción aquí y crea una respuesta adecuada
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
-
+    public int editarPerfil(@Valid @RequestBody DetallePacienteDTO detallePacienteDTO) throws Exception {
+        return pacienteServicio.editarPerfil(detallePacienteDTO);
     }
 
-    @DeleteMapping
-    public ResponseEntity<MensajeDTO> eliminarCuenta(@RequestParam int codigo) {
-        try {
-            pacienteServicio.eliminarCuenta(codigo);
-            return ResponseEntity.status(HttpStatus.OK).body(new MensajeDTO(
-                    false, "Paciente eliminado correctamente"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
+    @DeleteMapping("/eliminar/{codigo}")
+    public void eliminarCuenta(@RequestParam int codigo) throws Exception {
+        pacienteServicio.eliminarCuenta(codigo);
     }
 
     @GetMapping
-    public ResponseEntity<MensajeDTO> enviarLinkRecuperacion(@RequestParam String correo) {
-        try {
+    public void enviarLinkRecuperacion(@RequestParam String correo) throws Exception{
             pacienteServicio.enviarLinkRecuperacion(correo);
-            return ResponseEntity.status(HttpStatus.OK).body(new MensajeDTO(
-                    false, "Se ha enviado el link de recuperación de cuenta a su correo electronico," +
-                    "por favor revise en su bandeja de entrada o en spam"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
     }
 
     @GetMapping("/listaMedicosDisponibles")
-    public ResponseEntity<MensajeDTO> medicosDisponibles(@Valid @RequestBody MedicosDisponiblesDTO medicosDisponiblesDTO) {
-        try {
-            List<MedicosDisponiblesGetDTO> medicoPostDTOList = pacienteServicio.mostrarMedicosDisponibles(medicosDisponiblesDTO);
-            return ResponseEntity.status(HttpStatus.OK).body(new MensajeDTO(
-                    false, medicoPostDTOList));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
+    public List<MedicosDisponiblesGetDTO> medicosDisponibles(@Valid @RequestBody MedicosDisponiblesDTO medicosDisponiblesDTO) throws Exception {
+          return  pacienteServicio.mostrarMedicosDisponibles(medicosDisponiblesDTO);
     }
 
     @PostMapping("/cita")
-    public ResponseEntity<MensajeDTO> agendarCita(@Valid @RequestBody RegistroCitaDTO registroCitaDTO) {
-        try {
-            pacienteServicio.agendarCita(registroCitaDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new MensajeDTO(
-                    false, "Cita agendada correctamente"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
+    public int agendarCita(@Valid @RequestBody RegistroCitaDTO registroCitaDTO) throws Exception{
+            return  pacienteServicio.agendarCita(registroCitaDTO);
     }
 
     @PostMapping("/pqrs")
-    public ResponseEntity<MensajeDTO> crearPQRS(@Valid @RequestBody RegistroPQRSDTO registroPQRSDTO) {
-        try {
+    public void crearPQRS(@Valid @RequestBody RegistroPQRSDTO registroPQRSDTO) throws Exception{
             pacienteServicio.crearPQRS(registroPQRSDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new MensajeDTO(
-                    false, "Su PQRS se ha creado exitosamente"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO(
-                    true, e.getMessage()));
-        }
     }
 
 
