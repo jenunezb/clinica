4350380439207ba9b8ea16bde7b8f7b4eed487e6
jenunezb <jenunezb@uqvirtual.edu.co>
2023-10-23 21:50:25 -0500

    actualización de controladores para que el filtro token sirva

--- src/main/java/co/edu/uniquindio/proyecto/controladores/AdministradorController.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/AdministradorController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/administrador")
+@RequestMapping("api/admins")
 @AllArgsConstructor
 public class AdministradorController {
 
--- src/main/java/co/edu/uniquindio/proyecto/controladores/MedicoController.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/MedicoController.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.proyecto.controladores;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/medicos")
+@AllArgsConstructor
 public class MedicoController {
 }
